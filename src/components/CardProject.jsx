ef21922a04025914393e9eee06daa461194e642f
Mohamed <mkaouane1@gmail.com>
2021-05-04 21:25:21 +0200

    modification

--- src/components/CardProject.jsx
+++ src/components/CardProject.jsx
@@ -8,7 +8,7 @@ function CardProject() {
                 <span></span>
                 <div className="cardContent">
                     <h2>Projet 1</h2>
-                    <img src="https://via.placeholder.com/150" alt=""/><br />
+                    <img src="https://s3.gaming-cdn.com/images/products/3875/orig/project-winter-cover.jpg" style={{maxHeight: '250px', maxWidth: '250px'}} alt=""/><br />
                     <a href="#">Voir</a>
                 </div>
             </div>
@@ -16,7 +16,7 @@ function CardProject() {
                 <span></span>
                 <div className="cardContent">
                     <h2>Projet 1</h2>
-                    <img src="https://via.placeholder.com/150" alt=""/><br />
+                    <img src="https://s2.gaming-cdn.com/images/products/3250/orig/project-hospital-cover.jpg" style={{maxHeight: '250px', maxWidth: '250px'}} alt=""/><br />
                     <a href="#">Voir</a>
                 </div>
             </div>
@@ -24,7 +24,7 @@ function CardProject() {
                 <span></span>
                 <div className="cardContent">
                     <h2>Projet 1</h2>
-                    <img src="https://via.placeholder.com/150" alt=""/><br />
+                    <img src="https://www.nautiljon.com/images/anime/00/82/project_scard_praeter_no_kizu_9728.jpg" style={{maxHeight: '250px', maxWidth: '250px'}} alt=""/><br />
                     <a href="#">Voir</a>
                 </div>
             </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,11 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import './Navbar.css'
 import { Link } from "react-router-dom";
 
 
 function Navbar() {
+
+const [datacolor, setDatacolor] = useState('');
 
     useEffect(() => {
         let list = document.querySelectorAll('.navList');
@@ -17,23 +19,28 @@ function Navbar() {
             }
             
         }
-        list.forEach(elements => {
-            elements.addEventListener("click", (e) => {
-                let bg = document.querySelector('.sectionHeader');
-                let color = e.target.getAttribute('data-color');
-                bg.style.backgroundColor = color;
-            })
-        })
+        //  list.forEach(elements => {
+        //      elements.addEventListener("click", (e) => {
+        //          let bg = document.querySelector('.sectionHeader');
+        //          let color = e.target.getAttribute('data-color');
+        //          bg.style.backgroundColor = color;
+        //          setDatacolor(color)
+        //     })
+        //  })
         return () => {
-            
         };
     }, []);
+
+    const handleClick = event => {
+        let color = event.target.getAttribute('data-color');
+        console.log(color);
+    }
     
     return (
             <header>
             <Link to="/" className="logo" >Logo</Link> 
             <ul>
-                <li className="navList" data-color="#8a2435"><Link to="/" >Home</Link></li>
+                <li className="navList" onClick={handleClick} data-color="#8a2435"><Link to="/" >Home</Link></li>
                 
                 <li className="navList"data-color="#5280d6"><Link to="/plop">Projects</Link></li>
                 
